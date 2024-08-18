8221b7d28f4ef8b0b1b91b97bb3b4eda9a38f8e4
Jim Alpez <jim.alpez06@gmail.com>
2024-08-19 00:09:15 +0800

    commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
+          dolorum alias dicta quibusdam exercitationem culpa, vero perspiciatis
+          dolores, voluptate aliquam fugiat, eaque delectus itaque officiis
+          architecto tenetur accusamus iure natus!
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
