0c627810be74ecb6eb0e813c4248b551d8591baa
Jim Alpez <jim.alpez06@gmail.com>
2024-08-19 00:14:10 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/Navbar/navbar";
-import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
